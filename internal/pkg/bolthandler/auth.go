a0d79d7ee0d3fa93348d5cf9f02e47d6a09e83c1
Luis G. Villegas <luisguve@gmail.com>
2020-06-16 16:27:09 -0430

    implemented CheckUser and RegisterUser

---
+++ internal/pkg/bolthandler/auth.go
@@ -0,0 +1,167 @@
+package bolthandler
+
+import(
+	"log"
+	"errors"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+	bolt "go.etcd.io/bbolt"
+	pbDataFormat "github.com/luisguve/cheroproto-go/dataformat"
+)
+
+// CheckUser returns the user id associated with the given username, and true
+// if the given password and the stored password are equal, or an empty string
+// and false if either such username doesn't exist or the password check failed.
+func (h *handler) CheckUser(username, password string) (string, bool) {
+	userId, err := h.findUserIdByUsername(username)
+	if err != nil {
+		log.Println(err)
+		return "", false
+	}
+
+	userData := new(pbDataFormat.User)
+	err = h.users.View(func(tx *bolt.Tx) error {
+		usersBucket := tx.Bucket([]byte(usersB))
+		if usersBucket == nil {
+			return fmt.Errorf("Bucket %s of users not found\n", usersB)
+		}
+		userDataBytes := usersBucket.Get(userId)
+		if userDataBytes == nil {
+			log.Printf("Could not find user data (id %s)\n", string(userId))
+			return fmt.Errorf("User %s not found\n", string(userId))
+		}
+		err := proto.Unmarshal(userDataBytes, userData)
+		if err != nil {
+			log.Println("Could not unmarshal user")
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		log.Println(err)
+		return "", false
+	}
+	hashedPw := userData.PrivateData.Password
+	// check whether the provided password and the stored password are equal
+	err = bcrypt.CompareHashAndPassword(hashedPw, []byte(password))
+	if err != nil {
+		log.Println(err)
+		return "", false
+	}
+	// checked
+	return string(userId), true
+}
+
+// RegisterUser creates a new user with the provided data and returns the user id
+// of the just created and saved user and a nil *status.Status, or an empty string
+// and a given *status.Status indicating what went wrong: email or username already
+// in use, a database failure, an uuid or password hashing issue or a proto marshal
+// error.
+func (h *handler) RegisterUser(email, name, patillavatar, username, alias, about,
+	password string) (string, *status.Status) {
+	// check whether the username has been already taken
+	_, err := findUserIdByUsername(username)
+	// there must be an error, which should be ErrUsernameNotFound, otherwise the
+	// query could not be completed or the username has already been taken.
+	if err != nil {
+		if !errors.Is(err, ErrUsernameNotFound) {
+			return "", status.New(codes.Internal, "Failed to query database")
+		}
+	} else {
+		return "", status.New(codes.AlreadyExists, "Username already taken")
+	}
+
+	// check whether the email has been already taken
+	_, err := findUserIdByEmail(email)
+	// there must be an error, which should be ErrEmailNotFound, otherwise the
+	// query could not be completed or the email has already been taken.
+	if err != nil {
+		if !errors.Is(err, ErrEmailNotFound) {
+			return "", status.New(codes.Internal, "Failed to query database")
+		}
+	} else {
+		return "", status.New(codes.AlreadyExists, "Email already taken")
+	}
+
+	// generate universally unique identifier for the user id.
+	userIdBytes, err := uuid.NewV4()
+	if err != nil {
+		log.Printf("Could not get new uuid V4: %v\n", err)
+		return "", status.New(codes.Internal, "Could not generate user id")
+	}
+	userId := userIdBytes.String()
+
+	// hash password 10 times (Default cost)
+	hashedPw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Printf("Could not hash password \"%s\": %v\n", password, err)
+		return "", status.New(codes.Internal, "Could not generate password hash")
+	}
+
+	// format user in protobuf message
+	pbUser := &pbDataFormat.User{
+		BasicUserData: &pbDataFormat.BasicUserData{
+			Alias: alias,
+			Username: username,
+			PicUrl: patillavatar,
+			About: about,
+		},
+		PrivateData: &pbDataFormat.PrivateData{
+			Email: email,
+			Password: hashedPw,
+		},
+	}
+	// encode user into bytes
+	pbUserBytes, err := proto.Marshal(pbUser)
+	if err != nil {
+		log.Printf("Could not marshal user: %v\n", err)
+		return "", status.New(codes.Internal, "Could not marshal user")
+	}
+
+	// save user data
+	err = h.users.Update(func(tx *bolt.Tx) error {
+		// save user into users database
+		usersBucket := tx.Bucket([]byte(usersB))
+		if usersBucket == nil {
+			return fmt.Errorf("Bucket %s of users not found\n", usersB)
+		}
+		err := usersBucket.Put([]byte(userId), pbUserBytes)
+		if err != nil {
+			log.Printf("Could not put user: %v\n", err)
+			return err
+		}
+
+		// associate username to user id
+		usernamesBucket := tx.Bucket([]byte(usernamesB))
+		if usernamesBucket == nil {
+			log.Printf("Bucket %s of users not found\n", usernamesB)
+			return ErrBucketNotFound
+		}
+		err = usernamesBucket.Put([]byte(username), []byte(userId))
+		if err != nil {
+			log.Printf("Could not put username: %v\n", err)
+			return err
+		}
+
+		// associate email to user id
+		emailsBucket := tx.Bucket([]byte(emailsB))
+		if emailsBucket == nil {
+			log.Printf("Bucket %s of users not found\n", emailsB)
+			return ErrBucketNotFound
+		}
+		err = emailsBucket.Put([]byte(email), []byte(userId))
+		if err != nil {
+			log.Printf("Could not put user email: %v\n", err)
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		log.Println(err)
+		return "", status.New(codes.Internal, "Failed to query database")
+	}
+	return userId, nil
+}
--- internal/pkg/server/auth.go
+++ internal/pkg/server/auth.go
@@ -1,17 +1,50 @@
 package server
 
 import(
-	
+	"context"
+
+	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/codes"
+	pbApi "github.com/luisguve/cheroproto-go/cheroapi"
 )
 
 // Validate user credentials to login
 func (s *Server) Login(ctx context.Context,
 	req *pbApi.LoginRequest) (*pbApi.LoginResponse, error) {
-	
+	if s.dbHandler == nil {
+		return nil, status.Error(codes.Internal, "No database connection")
+	}
+
+	userId, ok := s.dbHandler.CheckUser(req.Username, req.Password)
+	if !ok {
+		return nil, status.Error(codes.PermissionDenied, "Invalid username or password")
+	}
+	return &pbApi.LoginResponse{
+		UserId: userId,
+	}, nil
 }
 
 // Register new user
 func (s *Server) RegisterUser(ctx context.Context,
 	req *pbApi.RegisterUserRequest) (*pbApi.RegisterUserResponse, error) {
+	if s.dbHandler == nil {
+		return nil, status.Error(codes.Internal, "No database connection")
+	}
 
+	email := req.Email
+	name := req.Name
+	patillavatar := req.PicUrl
+	username := req.Username
+	alias := req.Alias
+	about := req.About
+	password := req.Password
+
+	userId, st := s.dbHandler.RegisterUser(email, name, patillavatar, username,
+		alias, about, password)
+	if st != nil {
+		return nil, st.Err()
+	}
+	return &pbApi.RegisterUserResponse{
+		UserId: userId,
+	}, nil
 }
