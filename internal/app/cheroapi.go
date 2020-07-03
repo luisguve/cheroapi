fed14972dfea4d3fbebc134d9a45cc8c350b6f75
Luis G. Villegas <luisguve@gmail.com>
2020-07-02 19:52:26 -0430

    Implemented App structure and Run method.

--- internal/app/cheroapi.go
+++ internal/app/cheroapi.go
@@ -1,17 +1,31 @@
 package app
 
 import(
-	"log"
+	"net"
 
 	"google.golang.org/grpc"
-	"github.com/luisguve/cheroapi/internal/pkg/dbmodel"
+	"github.com/luisguve/cheroapi/internal/pkg/server"
+	pbApi "github.com/luisguve/cheroproto-go/cheroapi"
 )
 
-func New() *App {
+func New(s server.Server) *App {
 	return &App{
-		
+		srv: s,
 	}
 }
 
 type App struct {
+	srv server.Server
+}
+
+func (a *App) Run() error {
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		return fmt.Errorf("Failed to listen: %v\n", err)
+	}
+	s := grpc.NewServer()
+
+	pbApi.RegisterCrudCheropatillaServer(s, a.srv)
+
+	return s.Serve(lis)
 }
