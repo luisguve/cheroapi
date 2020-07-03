c43d66553b81800ba7269b73748a9ba2cfa32649
Luis G. Villegas <luisguve@gmail.com>
2020-07-02 19:52:09 -0430

    Implemented main function.

--- cmd/cheroapi.go
+++ cmd/cheroapi.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+
+	"github.com/luisguve/cheroapi/internal/pkg/server"
+	"github.com/luisguve/cheroapi/internal/pkg/bolt"
+	"github.com/luisguve/cheroapi/internal/app"
+)
+
+func main() {
+	h, err := bolt.New()
+	if err != nil {
+		log.Fatalf("Could not setup database: %v\n", err)
+	}
+	srv := server.New(h)
+	// Start App.
+	a := app.New(srv)
+	log.Fatal(a.Run())
+}
