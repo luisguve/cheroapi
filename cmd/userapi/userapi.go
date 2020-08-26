63fe5013d1e71378688ba4e59284c5207b5e5656
Luis G. Villegas <luisguve@gmail.com>
2020-08-26 13:58:25 -0430

    Updated main to get absolute path to .toml config file throuh command line flag "config".

--- cmd/userapi/userapi.go
+++ cmd/userapi/userapi.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"errors"
+	"flag"
 	"fmt"
 	"log"
-	"os"
-	"path/filepath"
 
 	"github.com/BurntSushi/toml"
-	"github.com/joho/godotenv"
 	app "github.com/luisguve/cheroapi/internal/app/userapi"
 	bolt "github.com/luisguve/cheroapi/internal/pkg/bolt/users"
 	server "github.com/luisguve/cheroapi/internal/pkg/server/users"
@@ -23,47 +20,41 @@ type cheroapiConfig struct {
 	SrvConf grpcConfig `toml:"users_grpc_config"`
 }
 
+func (c cheroapiConfig) preventDefault() error {
+	if c.DBdir == "" {
+		return fmt.Errorf("Missing db dir.")
+	}
+	if c.SrvConf.BindAddress == "" {
+		return fmt.Errorf("Missing users service bind address.")
+	}
+	return nil
+}
+
 func main() {
-	gopath, ok := os.LookupEnv("GOPATH")
-	if !ok || gopath == "" {
-		log.Fatal("GOPATH must be set.")
+	var configFile string
+	flag.StringVar(&configFile, "config", "", "Absolute path of .toml config file.")
+
+	flag.Parse()
+
+	if configFile == "" {
+		log.Fatal("Absolute path of .toml config file must be set.")
 	}
 
-	configDir := filepath.Join(gopath, "src", "github.com", "luisguve",
-		"cheroapi", "cheroapi.toml")
-
-	cheroConfig := new(cheroapiConfig)
-	if _, err := toml.DecodeFile(configDir, cheroConfig); err != nil {
+	config := cheroapiConfig{}
+	if _, err := toml.DecodeFile(configFile, &config); err != nil {
 		log.Fatal(err)
 	}
 
-	h, err := bolt.New(cheroConfig.DBdir)
+	if err := config.preventDefault(); err != nil {
+		log.Fatal(err)
+	}
+
+	dbHandler, err := bolt.New(config.DBdir)
 	if err != nil {
 		log.Fatalf("Could not setup database: %v\n", err)
 	}
-	srv := server.New(h)
+	srv := server.New(dbHandler)
 	// Start App.
 	a := app.New(srv)
-	log.Fatal(a.Run(cheroConfig.SrvConf.BindAddress))
-}
-
-func siteConfig(file string, vars ...string) (map[string]string, error) {
-	config, err := godotenv.Read(file)
-	if err != nil {
-		return nil, err
-	}
-	var result = make(map[string]string)
-	if len(vars) > 0 {
-		for _, key := range vars {
-			val, ok := config[key]
-			if !ok {
-				errMsg := fmt.Sprintf("Missing %s in %s.", key, file)
-				return nil, errors.New(errMsg)
-			}
-			result[key] = val
-		}
-	} else {
-		result = config
-	}
-	return result, nil
+	log.Fatal(a.Run(config.SrvConf.BindAddress))
 }
